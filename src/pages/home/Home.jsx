70a229b287923bdb1d72e924194444eea28b330c
Guilherme Nobrega <ghanobrega@gmail.com>
2025-06-17 18:04:22 +0100

    update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "../../Components/Navbar/Navbar";
+import styles from "./Home.module.css";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <div className={styles.container}>
+        <div className={styles.banner}></div>
+      </div>
     </div>
   );
 }
