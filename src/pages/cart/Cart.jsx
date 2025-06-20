0da553de7007a3b5d7b6ba0c29341629416269ac
Guilherme Nobrega <ghanobrega@gmail.com>
2025-06-20 12:57:15 +0100

    update

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,9 +1,54 @@
 import React from "react";
 import Navbar from "../../Components/Navbar/Navbar";
 import styles from "./Cart.module.css";
-import { TbShoppingBag } from "react-icons/tb";
+import {
+  TbMinus,
+  TbPlus,
+  TbShoppingBag,
+  TbShoppingCart,
+  TbTrash,
+} from "react-icons/tb";
 
 export default function Cart() {
+  const Item = () => {
+    return (
+      <div className={styles.item}>
+        <div className={styles.row}>
+          <div className={styles.itemImg}>
+            <img src="product.png" />
+          </div>
+          <div className={styles.columnProductInfo}>
+            <div className={styles.titleProduct}>
+              Banana biológica desidratada da Madeira
+            </div>
+            <div className={styles.rowBadges}>
+              <div className={styles.badge}>Peso: 100g</div>
+            </div>
+          </div>
+          <div className={styles.trash}>
+            <TbTrash />
+          </div>
+        </div>
+        <div className={styles.rows}>
+          <div className={styles.quant}>
+            <div className={styles.quantBtn}>
+              <TbMinus />
+            </div>
+            <div className={styles.quantTotal}>2</div>
+            <div className={styles.quantBtn}>
+              <TbPlus />
+            </div>
+          </div>
+          <div className={styles.columnActions}>
+            <div className={styles.columnPrices}>
+              <div>3.12€</div>
+              <div>1.56€ cada</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
   return (
     <div>
       <Navbar />
@@ -11,39 +56,39 @@ export default function Cart() {
         <div className={styles.separate}>
           <div className={styles.box}>
             <div className={styles.title}>
-              <TbShoppingBag />
-              Carrinho de compras
+              <TbShoppingCart />
+              Carrinho de compras ( 1 item )
             </div>
             <div className={styles.column}>
-              <div className={styles.item}>
-                <div className={styles.itemImg}>
-                  <img src="product.png" />
-                </div>
-              </div>
-              <div className={styles.item}>
-                <div className={styles.itemImg}>
-                  <img src="product.png" />
-                </div>
-              </div>
-              <div className={styles.item}>
-                <div className={styles.itemImg}>
-                  <img src="product.png" />
-                </div>
-              </div>
-              <div className={styles.item}>
-                <div className={styles.itemImg}>
-                  <img src="product.png" />
-                </div>
-              </div>
-              <div className={styles.item}>
-                <div className={styles.itemImg}>
-                  <img src="product.png" />
-                </div>
-              </div>
+              <Item />
             </div>
           </div>
-          <div className={styles.box}>
+          <div
+            className={`${styles.box} ${styles.sticky}`}
+            style={{ gap: ".75em" }}
+          >
             <div className={styles.title}>Resumo</div>
+            <div className={styles.rowBetween}>
+              <div>Subtotal</div>
+              <div>3.12€</div>
+            </div>
+            <div className={styles.rowBetween}>
+              <div>Portes de envio</div>
+              <div className={styles.free}>Grátis</div>
+            </div>
+            <div className={styles.rowBetween}>
+              <div>IVA</div>
+              <div>0.00€</div>
+            </div>
+            <div className={styles.totalInfo}>
+              <div className={styles.rowBetweenTotal}>
+                <div>Total</div>
+                <div>3.12€</div>
+              </div>
+              <div className={styles.checkoutBtns}>
+                <button className={styles.btnBuy}>Comprar agora</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
